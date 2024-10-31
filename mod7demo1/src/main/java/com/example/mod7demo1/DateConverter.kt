bdfea8ef181da9ec916de6c7c2b06e2df6be0b50
stropee <stropee@campus-eni.fr>
2024-10-31 14:05:02 +0100

    module 7 - demo 1 - api + exemple getsion de date avec le bon format

--- mod7demo1/src/main/java/com/example/mod7demo1/DateConverter.kt
+++ mod7demo1/src/main/java/com/example/mod7demo1/DateConverter.kt
@@ -1,13 +1,15 @@
 package com.example.mod7demo1
 
+import android.util.Log
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Date
 
+private const val TAG = "DateConverter"
 object DateConverter {
-    val formatter = SimpleDateFormat("dd/MM/yyyy")
+    val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S")
 
     @ToJson
     fun convertDateToSimpleString(date : Date): String {
@@ -18,9 +20,10 @@ object DateConverter {
     fun convertStringToSimpleDate(text : String): Date {
         var date = Date()
         try {
+
             date = formatter.parse(text)
         }catch (e: ParseException){
-
+            Log.i(TAG, "convertStringToSimpleDate: ${e.message}")
         }
         return date
     }
